cf4d12634cd5396f0d60d0d6792319b1b0d0c498
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-07 06:52:36 +0000

    Deploy Rahix/avr-hal to github.com/Rahix/avr-hal.git:gh-pages

---
+++ implementors/atmega32u4_hal/port/portf/trait.AsDDR.js
@@ -0,0 +1,4 @@
+(function() {var implementors = {};
+implementors["atmega32u4_hal"] = [];
+implementors["sparkfun_pro_micro"] = [{"text":"impl AsDDR for DDR","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
